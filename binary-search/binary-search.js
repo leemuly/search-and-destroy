37c972fc20e711486d122ef9bf6b564d1483fc80
Rebecca Rossman <rrossman25@gmail.com>
2019-07-19 11:40:25 -0400

    binary search 3 ways

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,24 @@
 'use strict';
+// n log(n) ===> recursive call
 
-// Complete this algo
-const binarySearch = (array, target) => {
-	
-};
+// const split = (arr) => {
+// 	const left = arr.slice(0, Math.ceil(arr.length / 2))
+// 	const right = arr.slice(Math.ceil(arr.length / 2))
+// 	return [left, right]
+// }
+
+// // Complete this algo
+// const binarySearch = (array, target) => {
+// 	if (array.length === 0){
+// 		return false
+// 	}
+// 	if (array.length === 1) {
+// 		return (array[0] === target)
+// 	}
+// 	const [left, right] = split(array);
+// 	return (binarySearch(left, target) || binarySearch(right, target))
+// };
+
 
 /*
 	EXTRA CREDIT:
@@ -13,5 +28,31 @@ const binarySearch = (array, target) => {
 	How can we use other pieces of data, like pointers, to avoid slicing?
 
 */
+
+// constant time ===> build in method
+
+// const binarySearch = (array, target) => {
+// 	return (array.includes(target));
+// }
+
+
+// n ===> looping
+
+const binarySearch = (array, target) => {
+
+	let beginningPointer = 0;
+	let endingPointer = array.length - 1;
+
+	while (beginningPointer < endingPointer){
+		if ((array[beginningPointer] === target || array[endingPointer] === target)){
+			return true
+		}
+
+		endingPointer--;
+		beginningPointer++;
+	}
+
+	return false
+}
 
 module.exports = binarySearch
