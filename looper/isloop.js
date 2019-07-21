858714e530d22f3bb99945aedbadc05b70169538
Marigrace Leeman <leemuly@gmail.com>
2019-07-21 00:55:14 -0400

    looper progress

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,9 +2,19 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  let listSet = new Set();
 
+  if (listSet.has(current)) {
+    return true;
+  } else {
+    listSet.add(current);
+    if (current.next !== null) {
+      return isLoop(linkedlist);
+    } else {
+      return false;
+    }
+  }
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +24,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
